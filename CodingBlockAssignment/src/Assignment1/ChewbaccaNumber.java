a26cef1676143500110b39842ac91a1b32c62448
nimeshHub <81302886+nimeshHub@users.noreply.github.com>
2022-01-19 13:44:31 +0530

    Update ChewbaccaNumber.java

--- CodingBlockAssignment/src/Assignment1/ChewbaccaNumber.java
+++ CodingBlockAssignment/src/Assignment1/ChewbaccaNumber.java
@@ -4,39 +4,40 @@ import java.util.*;
 import java.lang.*;
 
 public class ChewbaccaNumber {
-	static long reverse(long n) {
-		long temp = 0;
-		long d;
-		while(n!=0) {
-			d = n%10;
-			temp = temp*10 + d;
-			n = n/10;
-		}
-		return temp;
-	}
+// 	static long reverse(long n) {
+// 		long temp = 0;
+// 		long d;
+// 		while(n!=0) {
+// 			d = n%10;
+// 			temp = temp*10 + d;
+// 			n = n/10;
+// 		}
+// 		return temp;
+// 	}
 	public static void main(String[] args) {
-		Long n;
+		long n;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number");
 		n = sc.nextLong();
-		if(n>=0 && n<= (long)Math.pow(10, 18)) {
-			long rev = 0;
-			long x = 0;
-			long digit;
-			while(n!=0) {
-				digit = n%10;
-				x = 9 - digit;
-				if(x<digit) {
-					rev = rev*10 + x;
-				}
-				else if (x>digit) {
-					rev = rev*10 + digit;
-				}
-				n = n/10;
+	       // if(n>=0 && n<= (long)Math.pow(10,18)){
+		    long digit;
+		    long temp = 0;
+		    long mult= 1;
+		    long x;
+		    while(n!=0){
+			if(n==9){
+			    temp += 9*mult;
 			}
-			rev = reverse(rev);
-			System.out.println(rev);
-		}
+		       digit = n%10;
+		       if(digit>=5 && digit<=9){
+			   digit = 9-digit;
+		       }
+		       temp +=  digit*mult;
+			mult *=10;
+			n /=10;
+		    }
+		    //temp = reverse(temp);
+		    System.out.println(temp);
+	       // }
 		
 	}
 }

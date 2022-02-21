3057677d27fcbe33be3c92c99f28cfd8de5dde5a
nimeshHub <81302886+nimeshHub@users.noreply.github.com>
2022-02-22 00:22:11 +0530

    Update SquaresOfASortedArray.java

--- CodingBlockAssignment/src/Assignment3/SquaresOfASortedArray.java
+++ CodingBlockAssignment/src/Assignment3/SquaresOfASortedArray.java
@@ -1,38 +1,40 @@
-package Assignment3;
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.lang.Math;
-
-public class SquaresOfASortedArray {
-	public static void main(String[] args) {
-		int N;
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		if(N>=1 && N<=1000) {
-			int arr[] = new int[N];
-			for(int i = 0;i<N;i++) {
-				arr[i] = sc.nextInt();
-			}
-			int arr_square[] = new int[N];
-			for(int i = 0;i<N;i++) {
-				arr_square[i] = (int) Math.pow(arr[i], 2);
-			}
-			System.out.println("array->"+Arrays.toString(arr_square));
-			for(int i = 0;i<N-1;i++) {
-				int a = i;
-				int temp = 0;
-				for(int j = i + 1;j<N;j++) {
-					if(arr_square[j]<arr[i]) {
-						a = j;
-					}
-				}
-				temp = arr_square[i];
-				arr_square[i] = arr_square[a];
-				arr_square[a] = temp;
-				
-			}
-			System.out.println(Arrays.toString(arr_square));
-		}
-	}
+public class Main {
+    public static int[] sortArray(int arr[]) {
+        int n = arr.length;
+//      int sort_array[] = new int[n];
+        int temp = 0;
+        for(int i = 0;i<n;i++) {
+            for(int j = i +1;j<n;j++) {
+                if(arr[i]>arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int N;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        if(N>=1 && N<=10000) {
+            int arr[] = new int[N];
+            for(int i = 0;i<N;i++) {
+                arr[i] = sc.nextInt();
+            }
+            int arr_square[] = new int[N];
+            for(int i = 0;i<N;i++) {
+                arr_square[i] = (int) Math.pow(arr[i], 2);
+            }
+            arr = sortArray(arr_square);
+//          System.out.println(Arrays.toString(arr_square));
+            for(int i = 0;i<N;i++) {
+                System.out.print(arr_square[i]+" ");
+            }
+        }
+    }
 }

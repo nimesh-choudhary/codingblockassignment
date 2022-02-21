765da97b6d836aab8a7a96f578b1b5c421e654d3
nimeshHub <81302886+nimeshHub@users.noreply.github.com>
2022-02-21 23:21:41 +0530

    Update WavePrintRowWise.java

--- CodingBlockAssignment/src/Assignment3/WavePrintRowWise.java
+++ CodingBlockAssignment/src/Assignment3/WavePrintRowWise.java
@@ -1,18 +1,15 @@
-package Assignment3;
-
-import java.util.Scanner;
-
-public class WavePrintRowWise {
-	public static void print(int arr[][]) {
-		int r = arr.length;
-		int c = arr[0].length;
-		for(int i = 0;i<r;i++) {
-			for(int j =0;j<c;j++) {
-				System.out.print(arr[i][j]+" ");
-			}
-			System.out.println();
-		}
-	}
+import java.util.*;
+public class Main {
+    // public static void print(int arr[][]) {
+	// 	int r = arr.length;
+	// 	int c = arr[0].length;
+	// 	for(int i = 0;i<r;i++) {
+	// 		for(int j =0;j<c;j++) {
+	// 			System.out.print(arr[i][j]+" ");
+	// 		}
+	// 		System.out.println();
+	// 	}
+	// }
 	public static void wavePrint(int arr[][]){
 		int M = arr.length;
 		int N = arr[0].length;
@@ -44,23 +41,21 @@ public class WavePrintRowWise {
 						arr[i][j] = sc.nextInt();
 					}
 				}
-//				for(int i = 0;i<M;i++) {
-//					if(i%2 == 0) {
-//						for(int j = 0;j<M;j++) {
-//							System.out.print(arr[i][j]+" ");
-//						}
-//					}
-//					else {
-//						for(int j = N-1;j>=0;j--) {
-//							System.out.print(arr[i][j]+" ");
-//						}
-//					}
-//					System.out.println();
-//				}
-				wavePrint(arr);
+				for(int i = 0;i<M;i++) {
+					if(i%2 == 0) {
+						for(int j = 0;j<N;j++) {
+							System.out.print(arr[i][j]+", ");
+						}
+					}
+					else {
+						for(int j = N-1;j>=0;j--) {
+							System.out.print(arr[i][j]+", ");
+						}
+					}
+				}
+                System.out.print("END");
+				// wavePrint(arr);
 			}
 		}
-
-		
 	}
 }

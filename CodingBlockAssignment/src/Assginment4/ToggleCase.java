5a93b95c650ad227c55dc993f92e1eca0e28b05f
nimeshHub <81302886+nimeshHub@users.noreply.github.com>
2022-03-01 20:28:13 +0530

    Update ToggleCase.java

--- CodingBlockAssignment/src/Assginment4/ToggleCase.java
+++ CodingBlockAssignment/src/Assginment4/ToggleCase.java
@@ -1,10 +1,8 @@
-package Assginment4;
-
-import java.util.Scanner;
-
-public class ToggleCase {
-	public static void main(String[] args) {
-		String s;
+import java.util.*;
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        String s;
 		Scanner sc = new Scanner(System.in);
 		s = sc.next();
 		if(s.length()>=1 && s.length()<=1000) {
@@ -19,5 +17,5 @@ public class ToggleCase {
 			}
 			System.out.println(s_new);
 		}
-	}
+    }
 }

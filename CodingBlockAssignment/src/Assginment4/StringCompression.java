5c85c376009d9e77a83c9ef4711118b0d2adc07b
nimeshHub <81302886+nimeshHub@users.noreply.github.com>
2022-03-01 20:30:03 +0530

    Update StringCompression.java

--- CodingBlockAssignment/src/Assginment4/StringCompression.java
+++ CodingBlockAssignment/src/Assginment4/StringCompression.java
@@ -1,39 +1,38 @@
-package Assginment4;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int n = s.length();
+        int count = 0;
 
-import java.util.Scanner;
+        StringBuilder stringBuilder = new StringBuilder();
 
-public class StringCompression {
-	public static void main(String[] args) {
-		String s;
-		Scanner sc = new Scanner(System.in);
-		s = sc.next();
-		if(s.length()>=1 && s.length()<=1000) {
-			int count = 1;
-			StringBuilder sb = new StringBuilder();
-			for(int i = 1;i<s.length();i++) {
-				if(s.charAt(i) == s.charAt(i-1)) {
-					count++;
-				}
-				else {
-					sb.append(s.charAt(i-1));
-					sb.append(count);
-					count = 1;
-				}
-			}
-//			if(s.length() >1) {
-//				if(s.charAt(s.length()-1) == s.charAt(s.length()-2)) {
-//					count++;
-//				}
-//				else {
-//					sb.append(s.charAt(s.length()-2));
-//					sb.append(count);
-//					count = 1;
-//				}
-//				sb.append(s.charAt(s.length()- 1));
-//				sb.append(count);
-//			}
-			s = sb.toString();
-			System.out.println(s);
-		}
-	}
+        for (int i=0; i<n-1; i++){
+            char ch = s.charAt(i);
+            char next = s.charAt(i+1);
+
+            if (ch==next){
+                count++;
+            } else{
+                count++;
+                stringBuilder.append(ch);
+                if (count>1){
+                    stringBuilder.append(count);
+                }
+                count=0;
+            }
+        }
+
+        if (s.charAt(n-1)==s.charAt(n-2)){
+            count++;
+        }
+        stringBuilder.append(s.charAt(n-1));
+        if (count>1){
+            stringBuilder.append(count);
+        }
+
+        System.out.println(stringBuilder);
+    }
+
 }
